3763930c4a1c0ab2fbf64f712579d75309ae276f
huy923 <huyxam233@gmail.com>
2024-02-12 20:33:44 +0700

    sda

--- obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43e4c681dcf174dc7ec4b6ac3648ef8ec19ff987")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e7f76dfcabf282b8a6bb6d932d8116f99afaa68")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
